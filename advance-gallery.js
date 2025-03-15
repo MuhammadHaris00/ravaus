513770f4d67bd9ce3294fa267960190a6cd2df71
Muhammad Haris <69681086+MuhammadHaris00@users.noreply.github.com>
2025-03-16 01:41:50 +0500

    Checkpoint

--- advance-gallery.js
+++ advance-gallery.js
@@ -25,18 +25,21 @@ const popup_open_class = "popup-open";
 const popup_close_button = document.querySelector(".close-button");
 const popup_selected_image = document.querySelector(".gallery-popup-selected-image");
 let selected_image_rotation = 0;
+let selected_thumbnail_index = 0;
 const rotate_left_button = document.querySelector(".rotate-left-control");
 const rotate_right_button = document.querySelector(".rotate-right-control");
 const popup_zoom_button = document.querySelector(".double-zoom-control");
 const full_view_button = document.querySelector(".full-size-control");
-const popup_prev_button = document.querySelector(".popup-prev-button");
+const popup_prev_button = document.querySelector(".gallery-popup-prev-button");
+const popup_next_button = document.querySelector(".gallery-popup-next-button");
 const popup_zoom_canvas = document.querySelector(".gallery-popup-selected-image-canvas");
-const popup_zoom_area = 300;
+const popup_zoom_area = 250;
 const PopupZoomStates = {
     Disables:0,
-    SingleZoom:1,
-    DoubleZoom:2,
+    SingleZoom:0.5,
+    DoubleZoom:1,
 }
+let zoom_factor = PopupZoomStates.SingleZoom;
 
 
 popup_close_button.addEventListener("click", () => {
@@ -51,13 +54,14 @@ window.addEventListener("DOMContentLoaded",  () => {
             thumbnail_image.src = image;
             thumbnail_image.classList.add(thumbnail_image_classes);
             thumbnail_image.id = `thumbnail-image-${idx+1}`;
+            thumbnail_image.setAttribute("data-index", idx+"");
             images_wrapper.append(thumbnail_image);
 
             // Popup
             const popup_thumbnail = thumbnail_image.cloneNode(true);
             popup_thumbnail.classList.add(popup_thumbnail_class);
             popup_thumbnail.addEventListener("click", () => (
-                selectImage()
+                selectImage(popup_thumbnail, popup_zoom_canvas)
             ))
             popup_thumbnail.addEventListener("click", () => {
                 selectImage(popup_thumbnail, popup_selected_image);
@@ -82,7 +86,7 @@ window.addEventListener("DOMContentLoaded",  () => {
 
     if(zoom_canvas && selected_image) {
         selected_image.onmousemove = (e) => {
-            createZoom(e,selected_image,zoom_canvas,80,true,false);
+            createZoom(e,selected_image,zoom_canvas,80,1,true,false);
         }
     }
 
@@ -95,36 +99,72 @@ window.addEventListener("DOMContentLoaded",  () => {
         rotateSelectedImage(90);
     })
 
-    if(popup_zoom_canvas && popup_selected_image){
-        console.log("Hello", popup_selected_image);
-        popup_selected_image.onmousemove = (e)=>{
-            createZoom(e,popup_selected_image,popup_zoom_canvas,popup_zoom_area, false, true,0.5);
+    popup_zoom_button.addEventListener("click", () => {
+        changeZoomState();
+    })
+
+    full_view_button.addEventListener("click", () => {
+        window.open(selected_image.src, "_blank");
+    })
+
+    popup_prev_button?.addEventListener("click", () => {
+        const prevSibling = selected_thumbnail.previousElementSibling;
+        if (prevSibling && prevSibling.classList.contains("image-thumbnail")) {
+            selectImage(prevSibling, popup_selected_image);
         }
+    });
+    popup_next_button?.addEventListener("click", () => {
+        const nextSibling = selected_thumbnail.nextElementSibling;
+        if (nextSibling && nextSibling.classList.contains("image-thumbnail")) {
+            selectImage(nextSibling, popup_selected_image);
+        }
+    });
+
+
+    if(popup_zoom_canvas && popup_selected_image){
+        // console.log("Hello", popup_selected_image);
+        popup_selected_image.onmousemove = (e)=>{
+            createZoom(e,popup_selected_image,popup_zoom_canvas,popup_zoom_area,popup_zoom_area, false, true,zoom_factor);
+        }
+        popup_selected_image.addEventListener("click", (e)=>{
+            e.preventDefault();
+            changeZoomState();
+        })
     }
 
 })
 
-function createZoom(event,source_image, canvas_element,zoom_area_size = 100, highlight_area = false,canvas_follows = false, scale_factor = 1){
+function createZoom(event,source_image, canvas_element,zoom_area_size = 100,canvas_size = 100, highlight_area = false,canvas_follows = false, scale_factor = 1){
 
     if(source_image && canvas_element && zoom_area_size > 0) {
         const rect = event.target.getBoundingClientRect();
-        const mouse_pos_x = event.clientX - rect.left - zoom_area_size / 2;
-        const mouse_pos_y = event.clientY - rect.top - zoom_area_size / 2;
+        const source_rect = source_image.getBoundingClientRect();
+        const canvas_rect = canvas_element.getBoundingClientRect();
+        const mouse_pos_x = event.clientX - rect.left;
+        const mouse_pos_y = event.clientY - rect.top;
+
         // console.log(mouse_pos_x, mouse_pos_y);
 
-
+        if(scale_factor === 0){
+            popup_zoom_canvas.style.display = "none";
+        }
+        else {
+            popup_zoom_canvas.style.display = "block";
+        }
 
         //Calculating the Zoom Highlight Area Position
-        const left_pos = mouse_pos_x;
-        const top_pos = mouse_pos_y;
+        const left_pos = mouse_pos_x - zoom_area_size/2;
+        const top_pos = mouse_pos_y - zoom_area_size/2;
+        // const left_pos = mouse_pos_x;
+        // const top_pos = mouse_pos_y;
 
         if(highlight_area){
             zoom_highlight_area.style.width = `${zoom_area_size}px`;
             zoom_highlight_area.style.height = `${zoom_area_size}px`;
-            if(left_pos>0 && left_pos < source_image.width - zoom_area_size){
+            if(left_pos>0 && left_pos + zoom_area_size < source_image.width){
                 zoom_highlight_area.style.left = `${left_pos}px`;
             }
-            if(top_pos>0 && top_pos < source_image.height - zoom_area_size){
+            if(top_pos>0 && top_pos - zoom_area_size< source_image.height){
                 zoom_highlight_area.style.top = `${top_pos}px`;
             }
         }
@@ -132,8 +172,10 @@ function createZoom(event,source_image, canvas_element,zoom_area_size = 100, hig
         // Canvas Follows
 
         if(canvas_follows){
-            canvas_element.style.top = `${mouse_pos_y}px`;
-            canvas_element.style.left = `${mouse_pos_x}px`;
+            canvas_element.style.height = canvas_size + "px";
+            canvas_element.style.width = canvas_size + "px";
+            canvas_element.style.top = `${mouse_pos_y - canvas_rect.width/2}px`;
+            canvas_element.style.left = `${mouse_pos_x - canvas_rect.height/2}px`;
             // console.log(zoom_highlight_area.style.left);
         }
 
@@ -142,29 +184,24 @@ function createZoom(event,source_image, canvas_element,zoom_area_size = 100, hig
 
         // Setting the Zoom Background Size
         canvas_element.style.backgroundImage = `url(${source_image.src})`;
-        const source_rect = source_image.getBoundingClientRect();
-        const canvas_rect = canvas_element.getBoundingClientRect();
+
         // const zoom_canvas_image_size = source_rect.width * (canvas_rect.width / zoom_area_size);
-        const scale_ratio = source_rect.width/zoom_area_size;
-        const zoom_canvas_image_size = scale_ratio * source_rect.width;
-        canvas_element.style.backgroundSize = zoom_canvas_image_size*scale_factor + "px";
-        const bg_pos_x =-1*(mouse_pos_x * scale_ratio);
+        const scale_ratio_x = zoom_area_size/source_rect.width;
+        const scale_ratio_y = zoom_area_size/source_rect.height;
+        const zoom_canvas_image_size_x = source_rect.width/scale_ratio_x * scale_factor;
+        const zoom_canvas_image_size_y = source_rect.height/scale_ratio_y * scale_factor;
+        canvas_element.style.backgroundSize = `${zoom_canvas_image_size_x}px ${zoom_canvas_image_size_y}px`;
+        const bg_pos_x =-1*((mouse_pos_x/source_rect.width) * zoom_canvas_image_size_x);
         // Need to Know WHy 2.8 🤔
-        const bg_pos_y = -1*(mouse_pos_y * scale_ratio);
-        canvas_element.style.backgroundPositionX = `${bg_pos_x}px`;
-        canvas_element.style.backgroundPositionY = `${bg_pos_y}px`;
-        console.log(bg_pos_x, bg_pos_y, (mouse_pos_x/source_rect.width) * zoom_canvas_image_size,(mouse_pos_y/source_rect.height) * zoom_canvas_image_size);
-        // console.log(bg_pos_x,bg_pos_y);
-        // const bg_pos_x = zoom_canvas_image_size - (mouse_pos_x/source_image.width + zoom_area_size) * zoom_canvas_image_size;
-        // const bg_pos_y = zoom_canvas_image_size - (mouse_pos_y/source_image.height + zoom_area_size)  * zoom_canvas_image_size;
+        const bg_pos_y = -1*((mouse_pos_y/source_rect.height) * zoom_canvas_image_size_y);
+            canvas_element.style.backgroundPositionX = `${bg_pos_x + canvas_rect.width/2}px`;
+        if(mouse_pos_x+zoom_area_size<source_rect.width && mouse_pos_x-zoom_area_size/2>0){
+        }
+            canvas_element.style.backgroundPositionY = `${bg_pos_y + canvas_rect.height/2}px`;
+        if(mouse_pos_y+zoom_area_size<source_rect.height && mouse_pos_y-zoom_area_size/2>0){
+        }
+        // console.log((mouse_pos_x/source_rect.width),(mouse_pos_y/source_rect.height));
 
-        // if(left_pos>0 && left_pos < source_image.width){
-        // }
-        // if(top_pos>0 && top_pos < source_image.height){
-        // }
-
-        // target_element.style.backgroundPosition = `${bg_pos_x}px ${bg_pos_y}px`;
-        // console.log(bg_pos_x,bg_pos_y, zoom_canvas_image_size);
 
         //=====================================================
     }
@@ -184,8 +221,10 @@ function selectImage(new_image, canvas_element){
         selected_thumbnail = new_image;
         selected_thumbnail.classList.add(selected_thumbnail_classes);
         canvas_element.src = new_image.src;
+        console.log(canvas_element.src, new_image.src);
     }
     else{
+        console.log(new_image, canvas_element);
         throw new Error("Invalid New Image Parameter");
     }
 }
@@ -201,5 +240,17 @@ function rotateSelectedImage(angle){
     selected_image_rotation = (selected_image_rotation + angle)%360;
     popup_selected_image.style.transform = `rotate(${selected_image_rotation}deg)`;
     console.log(selected_image_rotation);
+}
+
+function changeZoomState(){
+    if(zoom_factor === PopupZoomStates.SingleZoom){
+        zoom_factor = PopupZoomStates.DoubleZoom;
+    }
+    else if(zoom_factor === PopupZoomStates.DoubleZoom){
+        zoom_factor = PopupZoomStates.Disables;
+    }
+    else{
+        zoom_factor = PopupZoomStates.SingleZoom;
+    }
 }
 
